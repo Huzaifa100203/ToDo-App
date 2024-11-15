77afde06fab0c538a89bd3a745b674056fe0718a
Huzaifa Mill <huzefafm53786@gmail.com>
2024-11-15 12:34:44 +0530

    second-commit

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
 const form = document.querySelector('form')
-const input = document.querySelector('input')
+const inputname = document.querySelector('#username')
+const inputemail = document.querySelector('#useremail')
+const inputphone = document.querySelector('#userphone')
 const ul = document.querySelector('ul')
 form.addEventListener('submit', (e) => {
     e.preventDefault()
-
-    // console.log("this is event")
-    list = document.createElement('li')
-    list.innerText = input.value
-    ul.appendChild(list)
-    list.className = "list-group-item w-75"
+    const card = document.createElement("div")
+    card.innerHTML = `
+     <h3>Name : ${inputname.value}</h3>
+     <p>Email : ${inputemail.value}</p>
+     <p>Phone number : ${inputphone.value}</p>
+     `
+     ul.appendChild(card)
+    card.style.width = "auto"
+    card.style.padding = "30px"
     const dltBtn = document.createElement('button')
     dltBtn.innerText = "Delete"
     dltBtn.className = "btn btn-danger float-end"
-    list.appendChild(dltBtn)
+    card.appendChild(dltBtn)
+    // list = document.createElement('li')
+    // list.innerText = input.value
+    // ul.appendChild(list)
+    // list.className = "list-group-item w-75"
     // dltBtn.addEventListener("click", (e)=>{
     //     // e.target = ul.remove()
-    //     form.remove(ul)
-    // })
-
+    //     ul.remove()
+    // }) this removes the whole list and does'nt allow you to add another list anymore
+    // console.log(card.children)
     form.reset()
 })
 function DeleteTask (e)  {
      if(e.target.className.includes("btn")){
         li = e.target.parentElement
+        console.log(li)
         li.remove()
      }
 }
